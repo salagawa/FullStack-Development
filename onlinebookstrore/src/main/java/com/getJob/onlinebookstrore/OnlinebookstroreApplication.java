72f733e983400c725b1ec47b41de3a36d088fb88
salagawa <shantala.c.a@gmail.com>
2021-07-20 12:31:02 +0530

    initial commit

---
+++ onlinebookstrore/src/main/java/com/getJob/onlinebookstrore/OnlinebookstroreApplication.java
@@ -0,0 +1,13 @@
+package com.getJob.onlinebookstrore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookstroreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookstroreApplication.class, args);
+	}
+
+}
---
+++ onlinebookstrore/src/test/java/com/getJob/onlinebookstrore/OnlinebookstroreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getJob.onlinebookstrore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookstroreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
